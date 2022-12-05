dae61b972704e0e6d613dcd4ca1b62c216b40d72
MattScop <matteo.scopece@outlook.com>
2022-12-05 08:36:57 +0100

    Initialize classes & add 'Append' method

---
+++ lib/linked_list.rb
@@ -0,0 +1,18 @@
+require_relative 'node.rb'
+
+class LinkedList
+  attr_accessor :head
+
+  def initialize
+    @head = nil
+  end
+
+  def append(value)
+    new_node = Node.new(value)
+    return @head = new_node if head.nil? #first_value
+
+    @head = head.next_node until head.next_node.nil?
+    #we are at the end
+    head.next_node = new_node
+  end
+end
---
+++ lib/main.rb
@@ -0,0 +1,13 @@
+require_relative 'linked_list.rb'
+
+linked_list = LinkedList.new
+
+# APPEND
+# linked_list.append(10)
+# puts linked_list.head.value
+# linked_list.append(20)
+# puts linked_list.head.next_node.value
+# linked_list.append(30)
+# puts linked_list.head.next_node.value
+# linked_list.append(40)
+# puts linked_list.head.next_node.value
